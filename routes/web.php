0a0c82e138884d12b6633c2fb27a9c7afb33fd33
dev-hasanmahmud <engr.hasanmahmud.bd@gmail.com>
2023-06-07 19:23:11 +0600

    Basic Routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Basic Routing
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
+ 
+Route::get('/user', [UserController::class, 'index']);
