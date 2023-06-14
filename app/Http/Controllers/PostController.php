bd7d43bc0a8dbe95b9f886a6fd749ed265cb3f92
dev-hasanmahmud <engr.hasanmahmud.bd@gmail.com>
2023-06-14 18:29:00 +0600

    Form Request Validation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\Post;
+use App\Http\Requests\StorePostRequest;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -10,7 +11,7 @@ class PostController extends Controller
         return view('pages.create');
     }
 
-    public function store(Request $request){
+    public function store(StorePostRequest $request){
         $request->validate([
             'title' => 'required|unique:posts|max:255',
             'descriptions' => 'required',
