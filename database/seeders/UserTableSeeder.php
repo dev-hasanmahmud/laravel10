d31ffc39396c56ba8bd00e1aec58fe7e32907883
dev-hasanmahmud <engr.hasanmahmud.bd@gmail.com>
2023-06-21 12:50:42 +0600

    Create and Calling Seeder

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'id'             => 1,
+                'name'           => 'Admin',
+                'email'          => 'admin@admin.com',
+                'password'       => bcrypt('password'),
+                'isAdmin'        => 1,
+                'remember_token' => null,
+            ],
+            [
+                'id'             => 2,
+                'name'           => 'User',
+                'email'          => 'user@user.com',
+                'password'       => bcrypt('password'),
+                'isAdmin'        => null,
+                'remember_token' => null,
+            ],
+        ];
+
+        User::insert($users);
+    }
+}
